71ae4c09fe98077b26974178f89b98c35731153f
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2023-03-02 13:28:12 +0200

    added the loader

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { Audio } from "react-loader-spinner";
 
 export default function Weather(props) {
   function handleResponse(response) {
@@ -11,5 +12,15 @@ export default function Weather(props) {
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
   axios.get(url).then(handleResponse);
 
-  return <h2>Hello from Weather</h2>;
+  return (
+    <Audio
+      height="80"
+      width="80"
+      radius="9"
+      color="green"
+      ariaLabel="loading"
+      wrapperStyle
+      wrapperClass
+    />
+  );
 }
