72d54f6cd78f13ddc63cac3b42875d61b28a7648
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2023-03-02 14:10:02 +0200

    moved weather project to vs code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import "./App.css";
+import "./Weather.css";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
-        <Weather city="London" />
-      </header>
+      <Weather />
     </div>
   );
 }
